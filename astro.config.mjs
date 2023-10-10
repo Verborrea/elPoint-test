71601e6ba8772feeba0e45517810285c89104902
Alan Monroy Bernedo <alan.monroy@ucsp.edu.pe>
2023-10-09 19:03:20 -0500

    fix url

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://Verborrea.github.io',
+    site: 'https://verborrea.github.io',
     base: '/elPoint-test',
 });
